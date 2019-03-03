11fe4b82ad59c0b1ff86a8a97ba307753ae49abb
chaithanayareddy-n <43605256+chaithanayareddy-n@users.noreply.github.com>
2019-03-03 22:59:19 +0530

    Update MithunTechnologiesController.java

--- multimodule-web/src/main/java/com/mt/controller/MithunTechnologiesController.java
+++ multimodule-web/src/main/java/com/mt/controller/MithunTechnologiesController.java
@@ -20,7 +20,7 @@ public class MithunTechnologiesController {
 			throws JSONException {
 		System.out.println(":::Method calling:::");
 		JSONObject js = new JSONObject();
-		js.put("Name", "Chaithanya Reddy L");
+		js.put("Name", "Chai Reddy L");
 		js.put("Technology", "DevOps  ");
 
 		return js.toString();
